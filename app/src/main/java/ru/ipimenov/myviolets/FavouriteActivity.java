07baeb2ab9303344e14accc2a7ba6f33137bfd70
Igor Pimenov <igor@ipimenov.ru>
2020-05-02 11:58:14 +0300

    Добавлена FavouriteViolet

---
+++ app/src/main/java/ru/ipimenov/myviolets/FavouriteActivity.java
@@ -0,0 +1,14 @@
+package ru.ipimenov.myviolets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FavouriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourite);
+    }
+}
